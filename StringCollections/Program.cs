9f3c104e3fa78f682de6e79a28927aa70966d97c
Pninja12 <pauloalmeida1288@gmail.com>
2023-05-17 15:38:16 +0100

    Exercício 2 concluído, com bastante dificuldade na criação do construtor e de alterar a ordem da lista

---
+++ StringCollections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Pedro", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name}!");
+            }
+        }
+    }
+}
