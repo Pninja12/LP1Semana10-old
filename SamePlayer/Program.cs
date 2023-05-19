031bc3ac2410d71e97217ed14b1bbf1415ee9355
Pninja12 <pauloalmeida1288@gmail.com>
2023-05-19 15:57:02 +0100

    Início do exercício 6

--- SamePlayer/Program.cs
+++ SamePlayer/Program.cs
@@ -4,16 +4,38 @@ namespace SamePlayer
 {
     public enum PlayerType {Tank , Fighter, Slayer, Mage, Controller, Marksmen}
 
-    public class Player{
-        public PlayerType Type {get; set; }
-        public string Name {get; set;}
-    }
-
-    class Program
+    public class Player
     {
-        static void Main(string[] args)
+        public PlayerType Type { get; set; }
+        public string Name { get; set; }
+
+        public Player(PlayerType type, string name)
         {
-            Console.WriteLine("Hello, World!");
+            Type = type;
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Player);
+        }
+
+        public bool Equals(Player p)
+        {
+            if (p != null)
+                return (this.Name == p.Name) && (this.Type == p.Type);
+
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Type, Name);
+        }
+
+        private static void Main(string[] args)
+        {
+            
         }
     }
 }
